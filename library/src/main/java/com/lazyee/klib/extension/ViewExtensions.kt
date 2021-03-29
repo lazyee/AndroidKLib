a96baace2712f3544e38353aad5d18356c7db26c
leeorz <378229364@qq.com>
2021-03-29 14:44:37 +0800

    增加拓展方法

--- library/src/main/java/com/lazyee/klib/extension/ViewExtensions.kt
+++ library/src/main/java/com/lazyee/klib/extension/ViewExtensions.kt
@@ -1,12 +1,17 @@
 package com.lazyee.klib.extension
 
+import android.content.Context
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.TextUtils
 import android.text.style.ImageSpan
+import android.util.DisplayMetrics
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * @Author leeorz
@@ -85,3 +90,37 @@ fun TextView.addDrawable(startIndex:Int = 0, vararg resIds:Int){
  * 获取图片的标示符
  */
 private fun getDrawablePlaceHolderText(key:String):String = "[###IMAGEPLACEHOLDER[$key]###]"
+
+/**
+ * 移动指定position到中间
+ * @receiver RecyclerView
+ * @param position Int
+ * @param duration Int 滚动的毫秒数
+ */
+fun RecyclerView.scrollPositionToCenter(position:Int,duration:Int){
+    try {
+        val layoutManager = layoutManager as LinearLayoutManager
+        val scroller =  CenterSmoothScroller(context,duration)
+        scroller.targetPosition = if(position < 0) 0 else position
+        if(scroller.isRunning)return
+        layoutManager.startSmoothScroll(scroller)
+    }catch (e:Exception){
+        e.printStackTrace()
+        scrollToPosition(position)
+    }
+}
+
+private class CenterSmoothScroller(context: Context, private val duration:Int) : LinearSmoothScroller(context) {
+
+    override fun calculateTimeForDeceleration(dx: Int): Int {
+        return duration
+    }
+
+    override fun calculateDtToFit(viewStart: Int, viewEnd: Int, boxStart: Int, boxEnd: Int, snapPreference: Int): Int {
+        return (boxStart + (boxEnd - boxStart) / 2) - (viewStart + (viewEnd - viewStart) / 2)
+    }
+
+    override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics): Float {
+        return 50f / displayMetrics.densityDpi
+    }
+}
