08b6de88bba918c8a0d5324800b940fc5b3e2eb4
leeorz <378229364@qq.com>
2023-05-23 16:45:49 +0800

    增加ViewBinding的Dialog

---
+++ library/src/main/java/com/lazyee/klib/base/ViewBindingDialog.kt
@@ -0,0 +1,33 @@
+package com.lazyee.klib.base
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.lazyee.klib.util.ViewBindingUtils
+
+/**
+ * Author: leeorz
+ * Email: 378229364@qq.com
+ * Description:ViewBinding Dialog
+ * Date: 2023/5/23 16:23
+ */
+open class ViewBindingDialog<VB:ViewBinding> :Dialog{
+
+    constructor(context: Context):this(context,0)
+    constructor(context: Context,themeResId:Int):super(context,themeResId)
+    constructor(context: Context,cancelable:Boolean,cancelListener: DialogInterface.OnCancelListener):super(context,cancelable,cancelListener)
+
+    lateinit var mViewBinding: VB
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewBinding = ViewBindingUtils.getViewBinding(javaClass, layoutInflater)
+        setContentView(mViewBinding.root)
+        initView()
+    }
+
+    open fun initView(){
+
+    }
+}
