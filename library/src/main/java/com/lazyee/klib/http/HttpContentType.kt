21db85720f37628175130bb8e524b76128636a8c
leeorz <378229364@qq.com>
2023-06-12 14:30:12 +0800

    增加contentType

--- library/src/main/java/com/lazyee/klib/http/HttpContentType.kt
+++ library/src/main/java/com/lazyee/klib/http/HttpContentType.kt
@@ -9,4 +9,5 @@ object HttpContentType {
     const val APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded"
     const val APPLICATION_JSON = "application/json"
     const val MULTIPART_FORM_DATA = "multipart/form-data"
+    const val TEXT_PLAIN = "text/plain"
 }
