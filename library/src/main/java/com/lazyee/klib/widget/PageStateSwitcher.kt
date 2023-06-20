ada8b2a703c8151a39fce36523794a01d44568ec
leeorz <378229364@qq.com>
2023-06-20 16:42:06 +0800

    增加PageStateSwitcher通过代码设置view的方法

--- library/src/main/java/com/lazyee/klib/widget/PageStateSwitcher.kt
+++ library/src/main/java/com/lazyee/klib/widget/PageStateSwitcher.kt
@@ -24,12 +24,6 @@ open class PageStateSwitcher:FrameLayout {
         }
     }
 
-    private var contentViewLayoutId:Int? = null
-    private var loadViewLayoutId:Int? = null
-    private var networkErrorViewLayoutId:Int? = null
-    private var exceptionViewLayoutId:Int? = null
-    private var emptyViewLayoutId:Int? = null
-
     private var contentView: View? = null
     private var networkErrorView: View? = null
     private var loadingView: View? = null
@@ -48,27 +42,6 @@ open class PageStateSwitcher:FrameLayout {
         loadingView = findViewById(R.id.loadingView)
         emptyView = findViewById(R.id.emptyView)
         exceptionView = findViewById(R.id.exceptionView)
-
-        //通过代码配置
-        if(contentView == null){
-            contentView = inflateLayoutByLayoutId(contentViewLayoutId)
-        }
-
-        if(loadingView == null){
-            loadingView = inflateLayoutByLayoutId(loadViewLayoutId)
-        }
-
-        if(networkErrorView == null){
-            networkErrorView = inflateLayoutByLayoutId(networkErrorViewLayoutId)
-        }
-
-        if(exceptionView == null){
-            exceptionView = inflateLayoutByLayoutId(exceptionViewLayoutId)
-        }
-
-        if(emptyView == null){
-            emptyView = inflateLayoutByLayoutId(emptyViewLayoutId)
-        }
 
         //通过全局配置初始化view
         if(loadingView == null){
@@ -91,23 +64,28 @@ open class PageStateSwitcher:FrameLayout {
     }
 
     fun setContentViewLayoutId(layoutId: Int?){
-        this.contentViewLayoutId = layoutId
+        contentView?.run { removeView(this) }
+        contentView = inflateLayoutByLayoutId(layoutId)
     }
 
     fun setNetworkErrorViewLayoutId(layoutId: Int?){
-        this.networkErrorViewLayoutId = layoutId
+        networkErrorView?.run { removeView(this) }
+        networkErrorView = inflateLayoutByLayoutId(layoutId)
     }
 
     fun setExceptionViewLayoutId(layoutId: Int?){
-        this.exceptionViewLayoutId = layoutId
+        exceptionView?.run { removeView(this) }
+        exceptionView = inflateLayoutByLayoutId(layoutId)
     }
 
     fun setEmptyViewLayoutId(layoutId: Int?){
-        this.emptyViewLayoutId = layoutId
+        emptyView?.run { removeView(this) }
+        emptyView = inflateLayoutByLayoutId(layoutId)
     }
 
     fun setLoadingViewLayoutId(layoutId: Int?){
-        this.loadViewLayoutId = layoutId
+        loadingView?.run { removeView(this) }
+        loadingView = inflateLayoutByLayoutId(layoutId)
     }
 
 
