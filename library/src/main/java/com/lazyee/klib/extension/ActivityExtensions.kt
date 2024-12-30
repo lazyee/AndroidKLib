f4a379de105be82eea888f7aa60c2f8437fb3feb
Leeorz <378229364@qq.com>
2024-12-30 10:51:43 +0800

    增加exoplayer使用示例

--- library/src/main/java/com/lazyee/klib/extension/ActivityExtensions.kt
+++ library/src/main/java/com/lazyee/klib/extension/ActivityExtensions.kt
@@ -2,8 +2,12 @@ package com.lazyee.klib.extension
 
 import android.app.Activity
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Author: leeorz
@@ -20,4 +24,54 @@ fun Activity.hideKeyboard(view:View){
 fun Activity.showKeyboard(view:View){
     val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+}
+
+
+/**
+ * 进入全屏模式
+ * @isHideActionBar 是否隐藏actionBar 默认隐藏
+ */
+fun Activity.enterFullScreen(isHideActionBar:Boolean = true){
+    window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    if(!isHideActionBar)return
+    if(actionBar != null){
+        actionBar?.hide()
+        return
+    }
+    if(this is AppCompatActivity && supportActionBar != null){
+        supportActionBar?.hide()
+        return
+    }
+}
+
+/**
+ * 退出全屏模式
+ * @isShowActionBar 是否显示actionBar 默认显示
+ */
+fun Activity.exitFullScreen(isShowActionBar:Boolean = true){
+    window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    if(!isShowActionBar)return
+    if(actionBar != null){
+        actionBar?.show()
+        return
+    }
+
+    if(this is AppCompatActivity && supportActionBar != null){
+        supportActionBar?.show()
+        return
+    }
+}
+
+/**
+ * 切换到竖屏
+ */
+fun Activity.changeOrientationPortrait() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+}
+
+/**
+ * 切换到横屏
+ */
+fun Activity.changeOrientationLandscape() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 }
